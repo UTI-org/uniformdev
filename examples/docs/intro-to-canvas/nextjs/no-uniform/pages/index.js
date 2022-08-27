dffdf1d781c0384f9f07ebc8378bcf6f9fa65129
Roger Willis <roger@rwmd.net>
2022-08-27 08:12:35 -0700

    init commit

--- examples/docs/intro-to-canvas/nextjs/no-uniform/pages/index.js
+++ examples/docs/intro-to-canvas/nextjs/no-uniform/pages/index.js
@@ -1,13 +1,38 @@
-import Layout from "../src/components/Layout";
-
+import { CanvasClient } from "@uniformdev/canvas";
+import { Composition } from "@uniformdev/canvas-react";
+import LayoutCanvas from "../src/components/LayoutCanvas";
+import HomeLayout from "../src/components/HomeLayout";
 import content from "../content/content.json";
-
+import doEnhance from "../lib/enhancer";
+import resolveRenderer from "../lib/resolveRenderer";
+async function getComposition(slug) {
+  const client = new CanvasClient({
+    apiKey: process.env.UNIFORM_API_KEY,
+    projectId: process.env.UNIFORM_PROJECT_ID,
+  });
+  const { composition } = await client.getCompositionBySlug({
+    slug,
+  });
+  return composition;
+}
 export async function getStaticProps() {
   const slug = "/";
   const topic = content.find((e) => e.url == slug);
-  return { props: { fields: topic.fields } };
+  const composition = await getComposition(slug);
+  await doEnhance(composition);
+  //
+  //Return props for the home page that
+  //include the composition and content
+  //required by the page components.
+  return {
+    props: {
+      composition,
+      fields: topic.fields,
+    },
+  };
 }
-
-export default function Home({ fields }) {
-  return <Layout content={content} fields={fields} />;
+export default function Home({ composition, fields }) {
+  console.log(composition);
+  console.log(fields);
+  return <HomeLayout content={content} fields={fields} />;
 }
